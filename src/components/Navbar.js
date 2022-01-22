55b17341c987d36e80204132d746de2b8cfdc11d
Omar Pacheco <omarspacheco@gmail.com>
2022-01-22 01:55:56 -0500

    Nav styling updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -47,7 +47,7 @@ export default function Navbar() {
                     alt="Workflow"
                   />
                 </div>
-                <div className="hidden sm:block sm:ml-6">
+                <div className="hidden sm:block sm:ml-6 nav-items">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <a
@@ -67,7 +67,7 @@ export default function Navbar() {
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {/* Profile dropdown */}
-                <NavMenu />
+                {/* <NavMenu /> */}
               </div>
             </div>
           </div>
